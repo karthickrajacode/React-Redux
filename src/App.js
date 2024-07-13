83c5ebc9d3cf54b70463d2ab167746626d10409c
Karthick <kart87632@gmail.com>
2024-07-13 14:39:47 -0700

    ggh

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import Counter from './Features/Counters/Counter';
+
+function App() {
+    return (
+        <div className='App'>
+            {/* <Counter/> */}
+            {<h1>Hello</h1>}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Features/Counters/Counter.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import { increment, decrement, reset } from './CounterSlice';
+// import { useSelector, useDispatch } from "react-redux";
+
+// const Counter = () => {
+//     const count = useSelector((state) => state.counter.count);
+//     const dispatch = useDispatch()
+//     return (
+//         <section>
+//             <p>{count}</p>
+//             <div>
+//                 <button onClick={() => dispatch(increment())}>Add</button>
+//                 <button onClick={() => dispatch(decrement())}>Remove</button>
+//                 <button onClick={() => dispatch(reset())}>Reset</button>
+//             </div>
+//         </section>
+//     )
+// };
+
+// export default Counter;
